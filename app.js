c64e44d33fbd9341e94336b3c5b151573454191d
tinoMukaro <vamukaro3@gmail.com>
2025-10-21 13:00:45 +0200

    config

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { PORT } from './config/env.js';
 
 const app = express();
 
-app.listen(3000, () =>{
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
 
 export default app;
