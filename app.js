e0bba3b42acab9ddce58b803d62035298670e594
tinoMukaro <vamukaro3@gmail.com>
2025-10-22 13:39:01 +0200

    setup error middlware

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ import AuthRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscriptions.router.js';
 import userRouter from './routes/user.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 app.use('/api/v1/auth', AuthRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
 app.use('/api/v1/users', userRouter)
+
+app.use(errorMiddleware)
 
 app.listen(PORT, async () =>{
     console.log(`Server is running on http://localhost:${PORT}`);
