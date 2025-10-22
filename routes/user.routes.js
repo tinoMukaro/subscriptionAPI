be6c500e94fa4b05ad17b43e3a71337c46e870b2
tinoMukaro <vamukaro3@gmail.com>
2025-10-22 15:00:49 +0200

    auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', ()=>{});
+userRouter.get('/',authorize, getUsers);
 
-userRouter.get('/:id', ()=>{});
+userRouter.get('/:id',authorize, getUser);
 
 userRouter.post('/', ()=>{});
 
