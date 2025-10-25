a42907f6007f0e1563e14bb28afb4fe195a3d4fb
tinoMukaro <vamukaro3@gmail.com>
2025-10-25 22:54:04 +0200

    setup workflows and nodemailer

--- config/env.js
+++ config/env.js
@@ -8,6 +8,8 @@ export const { PORT,
      JWT_SECRET,
      JWT_EXPIRES_IN,
      ARCJET_KEY,
-     ARCJET_ENV
+     ARCJET_ENV,
+     QSTASH_TOKEN, QSTASH_URL,
+     EMAIL_PASSWORD
 
      } = process.env;
